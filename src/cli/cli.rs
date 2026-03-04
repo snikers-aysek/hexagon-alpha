afffcc52540b22fcd2b1ecb3eeb38bd240b3095e
snikers! <108850801+snikers-aysek@users.noreply.github.com>
2026-03-04 19:15:33 +0500

    Update cli.rs

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,3 +1,19 @@
+/*
+ * HEXAGON Honey Pot Antivirus (v0.0.6-alpha)
+ * Copyright (C) 2026 Bad4Ending
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+
 use crate::core::hexagon::Hexagon;
 use crate::utils::log::{hexagon_log, LogLevel};
 
